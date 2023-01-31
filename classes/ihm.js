819f8be34b7a28400844386e7b135eb2469d96a4
Antoine Feredye <aferedye@gmail.com>
2023-01-31 14:42:13 +0100

    Avancement

--- classes/ihm.js
+++ classes/ihm.js
@@ -4,10 +4,12 @@ export class Ihm {
     constructor(formulaire, retourVehicule) {
         this.vehicule = [
             {
-                immatriculation : "bf-554-cd"
+                immatriculation : "bf-554-cd",
+                heureDepot : new Date()
             },
             {
-                immatriculation : "te-666-te"
+                immatriculation : "te-666-te",
+                heureDepot : new Date()
             }
         ]
         this.formulaire = formulaire
@@ -15,33 +17,38 @@ export class Ihm {
     }
 
     demarrer() {
+        const message = document.querySelector('#message')
         this.formulaire.addEventListener('submit', (e) => {
             e.preventDefault();
             const input = document.querySelector("input[name='immatriculation']").value
-            const exist = this.verificationVehicule(input);
+            let exist = this.verificationVehicule(input);
 
-        this.ajoutVehicule(exist);
+            this.ajoutVehicule(exist, input, message);
+            exist = this.verificationVehicule(input);
+            this.retraitVehicule(exist, input, message);
+
+        setTimeout(() => {
+            message.textContent = ""
+        },3000)
         })
+        
+
+        
     }
 
-    ajoutVehicule(exist) {
+    ajoutVehicule(exist, input, message) {
 
         if(exist == true) {
             return message.textContent = "Le véhicule existe déjà"
         } 
         else if (exist == false){
             const heureDepot = new Date();
-            const message = document.querySelector('#message')
-            const vehicule = new Vehicule(immatriculation, heureDepot)
+            const vehicule = new Vehicule(input, heureDepot)
             this.vehicule.push(vehicule)
+            message.textContent = "Véhicule enregistré"
             console.log(this.vehicule)
-            return message.textContent = "Véhicule enregistré"
-            
         }
     }
-
-
-    
 
     verificationVehicule(immatriculationInput) {
 
@@ -51,12 +58,23 @@ export class Ihm {
             }
         }
             return false;
-        }
+    }
     
-    retraitVehicule() {
+    retraitVehicule(exist, input, message) {
         this.retourVehicule.addEventListener('click', (e) => {
+            console.log("fonctionne", exist)
             e.preventDefault();
+            let outTime = new Date()
+            if (exist == true) {
+                console.log("existe")
+                for (const i of this.vehicule) {
+                    console.log(((outTime-i.heureDepot)/1000))
+                    if (i.heureDepot <= ((outTime-i.heureDepot)/1000)) {
+                        console.log("OK")
 
+                    }
+                }
+            }
 
         })
     }
